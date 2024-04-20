a68f14220525eee10b993430f3b4150afb8ed916
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-04-21 00:59:21 +0600

    adding middleware js

---
+++ middleware.js
@@ -0,0 +1,12 @@
+module.exports = async (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust for specific origins if needed
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allowed methods
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allowed headers
+  
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+    } else {
+      next(); // Pass the request to your actual API endpoints
+    }
+  };
+  
